bd07ba061ca2d8bd599a028392ffd1f7b27342e4
Pavan Boro <pavanboro14@gmail.com>
2018-01-30 18:52:49 +0530

    Done flipDigit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # My-Coding-Challenge
 This is the repository where I'll be uploading my daily programming challenge.
+
+## Data Structures:
+
+1. - [ ] Arrays
+2. - [ ] Stacks
+3. - [ ] Queues
+4. - [ ] Linked Lists
+5. - [ ] Trees
+6. - [ ] Graphs
+7. - [ ] Hashing
---
+++ flipDigit.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main(){
+    
+    int digits = 0;
+    int n_zero = 0, n_one = 0;
+    int t = 100;
+    
+// Finding the no of 1's and 0's
+    while (t >0) {
+        
+        n_zero = n_zero + (t%10>0 ? 0 : 1);
+        
+        // used to traverse all through the digit.
+        t = t/10;
+        digits++;
+    }
+    
+
+    n_one = digits - n_zero;
+    printf("\n n_zero: %d",n_zero);
+    printf("\n n_one: %d\n",n_one);
+
+   
+// Problem: Digits equal or not by flipping 1 digit
+    if (n_zero > n_one) {
+        if (n_one == 1) {
+            printf("yes");
+        } else {
+            printf("no");
+        }
+    } else {
+        if (n_zero == 1) {
+            printf("yes");
+        } else {
+            printf("no");
+        }
+    }
+    
+    return 0;
+}
